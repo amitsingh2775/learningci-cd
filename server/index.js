d28a996f845ecc2ff6cae1a8a29e61e5bffe9a38
amit kumar <107244837+amitsingh2775@users.noreply.github.com>
2024-12-26 16:40:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express=require('express')
-const app=express()
+const express = require('express');
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hii amit")
-})
+app.get('/', (req, res) => {
+    res.send("hii");
+});
 
-app.listen(3000,()=>{
-    console.log("server started");
-    
-})
+app.listen(3000, '0.0.0.0', () => {
+    console.log("server started on port 3000");
+});
